904b8d09a7662963ea1d474463d66eba89283c4f
kali <bhupeshdawadi12345@gmail.com>
2022-10-01 20:04:00 +0545

    update

--- hero.py
+++ hero.py
@@ -1,6 +1,4 @@
 import random
-
-
 import matplotlib.pyplot as plt
 import seaborn
 from sklearn.ensemble import RandomForestClassifier
@@ -21,9 +19,9 @@ drop = data.dropoff
 #     delta = ddate - pdate
 #     data['delivery'][i-1] = int(delta.total_seconds()/60)
 # new_data = pd.unique(data)
-model = LinearRegression()
-model.fit(data[['total']],data['delivery'])
-pred = model.predict(data[['total']])
+# model = LinearRegression()
+# model.fit(data[['total']],data['delivery'])
+# pred = model.predict(data[['total']])
 # print(model.coef_,model.intercept_)
 # print(accuracy_score(data.total.astype(int),pred.astype(int))
 # print(new_data)
@@ -35,9 +33,9 @@ x = (data[['passengers','distance','total']])
 x['colors'] = color
 y = data['payment'].fillna('credit card')
 y = pd.get_dummies(y,drop_first=True)
-x,xt,y, yt = train_test_split(x,y)
+# x,xt,y, yt = train_test_split(x,y)
 from acc_check import accuracy
-accuracy(x,xt,y, yt)
+# accuracy(x,xt,y, yt)
 # model = SVC()
 # model.fit(x,y)
 # pre = model.predict(xt)
@@ -69,16 +67,16 @@ accuracy(x,xt,y, yt)
 # pre = model.predict(xt)
 # print(accuracy_score(pre,yt))
 
-
-# from sklearn.cluster import KMeans
+fig, axs = plt.subplots(ncols=2, nrows=2)
+from sklearn.cluster import KMeans
 # import seaborn as se
 # import matplotlib.pyplot as plt
-# new_data = data[['total','distance','passengers']]
-# cluster = KMeans(n_clusters=3)
+new_data = data[['total','distance','passengers']]
+cluster = KMeans(n_clusters=10)
 # given = [0, 1, 2]
 # desired = ['low','medium','high']
-# cluster.fit(new_data)
-# new_data['label'] =(cluster.labels_)
+cluster.fit(new_data)
+new_data['label'] =(cluster.labels_)
 # low, high, med = new_data.groupby(by='label')
 # print(low)
 # print(med)
@@ -93,9 +91,8 @@ accuracy(x,xt,y, yt)
 # # print(pd.DataFrame(high[1]).describe().mean().total)
 # # print(pd.DataFrame(med[1]).describe().mean().total)
 # # print(low[1].describe().mean().total)
-# se.scatterplot(data['distance'],data['total'],hue=new_data['label'],s=15)
-# plt.style.use('ggplot')
-# plt.show()
+se.scatterplot(data['distance'],data['total'],hue=new_data['label'],s=5,ax=axs[(0,1)])
+
 
 
 # datas = se.get_dataset_names()
@@ -112,6 +109,20 @@ accuracy(x,xt,y, yt)
 # plt.tight_layout()
 # plt.grid()
 # plt.show()
+
+
+from sklearn.cluster import  AgglomerativeClustering
+agg = AgglomerativeClustering(n_clusters=10).fit(x).labels_
+print(len(agg))
+se.scatterplot(data['distance'],data['total'],hue=agg,s=5,ax=axs[(1,0)])
+plt.style.use('ggplot')
+plt.show()
+from sklearn.cluster import  Birch
+agg = Birch(threshold=1.7, n_clusters=10).fit(x).labels_
+se.scatterplot(data['distance'],data['total'],hue=agg,s=5,ax=axs[(1,1)])
+print('sucess')
+# plt.style.use('ggplot')
+plt.show()
 
 
 
